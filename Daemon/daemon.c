414ce18b9a7c84a6a44ac5a76e6197f95ca8d7ce
Airinei Andrei <acairinei17@gmail.com>
2023-01-16 14:37:45 +0200

    daemon.h

--- Daemon/daemon.c
+++ Daemon/daemon.c
@@ -1,17 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <syslog.h>
-#include <sys/stat.h>
-#include <signal.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <string.h>
-#include <errno.h>
-
-#include "../Shared/shared.h"
+#include "daemon.h"
 
 int SocketFD;
 struct sockaddr_in sa;
