b5d700b83e8d6d2efab12ded68041b706ed57b21
Airinei Andrei <acairinei17@gmail.com>
2023-01-16 14:45:42 +0200

    da.h

--- da/da.c
+++ da/da.c
@@ -1,20 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <signal.h>
-#include <errno.h>
-#include <inttypes.h>
-#include <fcntl.h>
-#include <unistd.h>
-
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-
-#include "../Shared/shared.h"
+#include "da.h"
 
 struct sockaddr_in sa;
 int SocketFD;
---
+++ da/da.h
@@ -0,0 +1,21 @@
+#ifndef DA_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <errno.h>
+#include <inttypes.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#include "../Shared/shared.h"
+
+#endif // DA_H
